df4f3dd0958213dc291312408459bcd2b2ea09f4
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-03 11:14:10 +0200

    first input

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
-const materialComponents = [MatButtonModule];
+const materialComponents = [MatButtonModule, MatInputModule];
 
 @NgModule({
     declarations: [],
